7bc5719a1b3f536e3964b693b1aecfe3179f78bd
kirtward <72439627+kirtward@users.noreply.github.com>
2020-12-16 10:14:21 +0000

    Add files via upload
    Basic random password generator, 8 characters (2 upper, 2 lower, 2 digits & 2 special)

---
+++ PasswordGen_v1.0.py
@@ -0,0 +1,30 @@
+import random
+
+# shuffle password
+def shuffle(string):
+	tempList = list(string)
+	random.shuffle(tempList)
+	return ''.join(tempList) 
+	
+# Upcase section creation
+uc1=chr(random.randint(65,90))
+uc2=chr(random.randint(65,90))
+
+# Lowercase section creation
+lc1=chr(random.randint(97,122))
+lc2=chr(random.randint(97,122))
+
+# Digit section creation
+d1=chr(random.randint(48,57))
+d2=chr(random.randint(48,57))
+
+# Special Character 
+s1=chr(random.randint(33,47))
+s2=chr(random.randint(33,47))
+
+# Create whole password and pass to shuffle
+password = uc1 + uc2 + lc1 + lc2 + d1 + d2 + s1 + s2
+password = shuffle(password)
+
+#Output
+print(password)
